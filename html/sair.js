82eac7c5ee4ae72d24be987b99ea8c95fe1f5653
caua-pereira-santos <cauap600@gmail.com>
2023-04-23 16:57:11 -0300

    Add: replaceState

--- html/sair.js
+++ html/sair.js
@@ -1,9 +1,28 @@
-// adicionando um listener de eventos ao botão de saída
-document.getElementById("btn-sair").addEventListener("click", function() {
-    // limpar dados do usuário (opcional)
-    // ...
-  
-    // redirecionar o usuário para a página de login ou de saída
-    window.location.href = "pagina-de-login-ou-saida.html";
-  });
-  
+
+/*
+    // verifica se o usuário está logado
+if (!sessionStorage.getItem('usuario') || !sessionStorage.getItem('senha')) {
+    location.href = 'paginaLogin.html'; // redireciona o usuário para a página de login se ele não estiver logado
+}
+
+// adiciona um listener para o evento popstate
+window.addEventListener('popstate', function(event) {
+    if (event.state && event.state.from === 'paginaInicial') {
+        history.go(-1); // volta para a página anterior
+        setTimeout(function() {
+            history.replaceState({from: 'paginaInicial'}, '', ''); // substitui a entrada atual no histórico de navegação
+        }, 0);
+    }
+});
+
+//Adiciona um evento de clique no botão "Sair"
+let btnSair = document.getElementById('btnSair');
+
+btnSair.addEventListener('click', function() {
+    sessionStorage.removeItem('usuario');
+    sessionStorage.removeItem('senha');
+    history.replaceState({from: 'paginaInicial'}, '', 'index.html'); // substitui a entrada atual no histórico de navegação
+    location.href = 'paginaLogin.html'; // redireciona o usuário para a página de login
+});
+
+*/
