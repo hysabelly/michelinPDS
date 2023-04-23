44af87a364366e85286ab37ae55bec5cd82a7bc8
caua-pereira-santos <cauap600@gmail.com>
2023-04-23 15:12:33 -0300

    Add: Botão sair

---
+++ html/sair.js
@@ -0,0 +1,9 @@
+// adicionando um listener de eventos ao botão de saída
+document.getElementById("btn-sair").addEventListener("click", function() {
+    // limpar dados do usuário (opcional)
+    // ...
+  
+    // redirecionar o usuário para a página de login ou de saída
+    window.location.href = "pagina-de-login-ou-saida.html";
+  });
+  
