8cb8f9a247b06cfb9dc46ab6ef5b99ec414e38f1
GUSTAVOCHAVES4 <gustavonoronha854@gmail.com>
2023-04-25 04:18:25 -0300

    Update: pom.xml, target, web.xml; Add: servlet e pasta java

---
+++ src/main/java/com.michellin.servlets/MeuServlet.java
@@ -0,0 +1,16 @@
+package com.azure.michellin.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MeuServlet extends HttpServlet {
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    response.setContentType("text/html");
+    response.getWriter().println("<html><body><h1>Oi, Mundo!</h1></body></html>");
+  }
+}
