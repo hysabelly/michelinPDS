c1689d71b93b6ebb1fde9b57f99926506e60bf90
Emyzinha <101672818+Emyzinha@users.noreply.github.com>
2023-05-28 23:07:37 -0300

    atualização paraVoce.js

--- html/paraVoce.js
+++ html/paraVoce.js
@@ -1,36 +1,27 @@
-function mostrarProximaPergunta(pergunta) {
-    // Exibe a próxima pergunta
-    document.getElementById(pergunta).style.display = "block";
+function mostrarPergunta(perguntaId) {
+    var pergunta = document.getElementById(perguntaId);
+    pergunta.style.display = "block";
   }
   
-  function mostrarPergunta(pergunta) {
+  function mostrarPergunta(id) {
     // Exibe a pergunta selecionada e esconde as outras
     var perguntas = document.getElementsByClassName("form-group");
     for (var i = 0; i < perguntas.length; i++) {
       perguntas[i].style.display = "none";
     }
-    document.getElementById(pergunta).style.display = "block";
+    document.getElementById(id).style.display = "block";
   
     // Verifica se é a última pergunta
-    if (pergunta === "pergunta-3" || pergunta === "pergunta-8" || pergunta === "pergunta-11" || pergunta === "pergunta-15" || pergunta === "pergunta-17" || pergunta === "pergunta-19" || pergunta === "pergunta-21") {
+    if (id === "pergunta-3" || id === "pergunta-5" || id === "pergunta-7" || id === "pergunta-10" || id === "pergunta-11" || id === "pergunta-12" || id === "pergunta-9" || id === "pergunta-13" || id === "pergunta-15" || id === "pergunta-17" || id === "pergunta-19" || id === "pergunta-21" || id === "pergunta-22") {
       // Exibe a resposta final e interrompe o questionário
-      var resposta = document.getElementById(pergunta).getElementsByTagName("p")[0].innerText;
+      var resposta = document.getElementById(id).getElementsByTagName("p")[0].innerText;
       document.getElementById("resposta-final").innerHTML = "Sua resposta: " + resposta;
       document.getElementById("resposta-final").style.display = "block";
       document.getElementById("questionario").style.display = "none";
     }
   }
   
-  function mostrarErro() {
-    // Exibe uma mensagem de erro caso a resposta seja "Não"
-    document.getElementById("erro").style.display = "block";
-  }
-  
-  function mostrarPergunta(id) {
-    document.getElementById(id).style.display = "block";
-  }
-
   function mostrarProximaPergunta(id) {
     document.getElementById(id).style.display = "block";
-    window.scrollTo(0,document.body.scrollHeight);
+    window.scrollTo(0, document.body.scrollHeight);
   }
